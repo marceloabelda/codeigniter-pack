ab5aff83aa2ab12458ad91e3b6d5aa9d6e0a3a13
marceloabelda <marceloabelda@gmail.com>
2011-07-27 08:13:37 -0300

    otro

---
+++ themes/admin-theme/views/partials/aside.php
@@ -0,0 +1,8 @@
+<?php
+$list = array(
+            anchor('users/admin/index','Usuarios'),
+            anchor('groups/admin/index','Grupos'),
+            );
+echo ul($list);
+?>           
+            
